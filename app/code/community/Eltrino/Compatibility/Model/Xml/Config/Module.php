3233c1dc8d9bf2d370dac75c69230a7fa067629c
Vladimir Polischuk <vladimir.polischuk@eltrino.com>
2015-03-17 13:21:17 +0200

    add license

--- app/code/community/Eltrino/Compatibility/Model/Xml/Config/Module.php
+++ app/code/community/Eltrino/Compatibility/Model/Xml/Config/Module.php
@@ -1,5 +1,28 @@
 <?php
 
+/**
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2015 Eltrino LLC (http://eltrino.com)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
 class Eltrino_Compatibility_Model_Xml_Config_Module extends Mage_Core_Model_Config_Base
 {
     /** @var Mage_Core_Model_Config|null */
@@ -85,17 +108,6 @@ class Eltrino_Compatibility_Model_Xml_Config_Module extends Mage_Core_Model_Conf
 
         }
 
-        /**
-         * TODO: add check for data installer
-         *
-         * <resources>
-         *      <eltrino_test2_setup>
-         *          <setup>
-         *              <module>Eltrino_Test2</module>
-         *          </setup>
-         *      </eltrino_test2_setup>
-         * </resources>
-         */
         if (is_readable($this->_moduleDir . '/sql')) {
             /** @var Mage_Core_Model_Config_Element $resources */
             $resources = $globalNode->resources;
@@ -154,7 +166,8 @@ class Eltrino_Compatibility_Model_Xml_Config_Module extends Mage_Core_Model_Conf
                 );
                 $router->addChild('use', (string)$newConfig->getNode('router')->getAttribute('id'));
                 $router->addChild('args');
-                $router->args->addChild('frontName', (string)$newConfig->getNode('router/route')->getAttribute('frontName'));
+                $router->args->addChild('frontName',
+                    (string)$newConfig->getNode('router/route')->getAttribute('frontName'));
                 $router->args->addChild('module',
                     (string)$newConfig->getNode('router/route/module')->getAttribute('name')
                 );
--- app/code/community/Magento/Framework/View/Element/Template/Context.php
+++
@@ -1,7 +0,0 @@
-<?php
-namespace Magento\Framework\View\Element\Template;
-
-class Context_temp extends \Varien_Object
-{
-
-}
